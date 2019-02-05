6745ea3b6b00c25516b5890a48b04945f55889ed
Irina Delamare <irina.delamare@gmail.com>
2019-02-05 12:01:18 +0100

    Update Prime-number-calculator_1.py

--- Prime-number-calculator_1.py
+++ Prime-number-calculator_1.py
@@ -1,5 +1,4 @@
-a = input("Please enter the maximum number value : ")
-a = int(a)
+a = int(input("Please enter the maximum number value : "))
 non_premier = [i for i in range (0,a) for n in range(2,a) if n!=i and i%n==0]
 o = list(range(2, a))
 
